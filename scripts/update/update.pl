0c72cd4e7795d5b93e96f00da6ae7a57967f88ac
Nicholas Grogg <ngrogg@pop-os.localdomain>
2022-10-29 13:06:42 -1000

    First draft of update script

--- scripts/update/update.pl
+++ scripts/update/update.pl
@@ -8,27 +8,31 @@ sub runProgram {
 	## Check for package managers
 	### Check for apt
 	if (-e "/usr/bin/apt") {
-		print "Apt\n"
+		print "Apt\n";
+		system("sudo apt update -y && sudo apt upgrade -y");
 	}
 	
 	### Check for yum
 	elsif (-e "/usr/bin/yum") {
-		print "Yum\n"
+		print "Yum\n";
+		system("sudo yum update -y");
 	}
 
 	### Check for pacman
 	elsif (-e "/usr/bin/pacman") {
-		print "pacman\n"
+		print "pacman\n";
+		system("sudo pacman -Syu");
 	}
 
 	### Check for zypper
 	elsif (-e "/usr/bin/zypper") {
-		print "zypper\n"
+		print "zypper\n";
+		system("sudo zypper update");
 	}
 
 	### Else something else 
 	else {
-		print "Something else is installed, update manually"
+		print "Something else is installed, update manually";
 	}
 }
 
