6829d026db83162683da98ab422360c4b29fb43a
Nicholas Grogg <ngrogg@pop-os.localdomain>
2022-12-16 01:35:11 -1000

    Add distro agnostic code for flatpak/snap to update.pl

--- scripts/update/update.pl
+++ scripts/update/update.pl
@@ -7,42 +7,62 @@
 sub runProgram {
 	## Check for distro agnostic package installs
 	### Flatpak
+	print "Checking if flatpak installed\n";
+	print "-----------------------------------------\n";
 	#### Check if flatpak installed
-	#### If flatpak installed run update command 
+	if (-e "/usr/bin/flatpak") {
+		print "flatpak detected - updating";
+		print "-----------------------------------------\n";
+		#### If flatpak installed run update command 
+		system("flatpak update");
+	}
 	### Snap
 	#### Check if snap installed
-	#### If snap installed run update command 
+	print "Checking if snap installed\n";
+	print "-----------------------------------------\n";
+	if (-e "/usr/bin/snap") {
+		print "snap detected - updating";
+		print "-----------------------------------------\n";
+		#### If snap installed run update command 
+		system("sudo snap refresh");
+	}
 	
 	## Check for package managers
 	### Check for apt
 	if (-e "/usr/bin/apt") {
-		print "Apt\n";
+		print "Apt package manager detected\n";
+		print "-----------------------------------------\n";
 		system("sudo apt update && sudo apt upgrade");
 		system("sudo apt autoremove");
 	}
 	
 	### Check for yum
 	elsif (-e "/usr/bin/yum") {
-		print "Yum\n";
+		print "Yum package manager detected\n";
+		print "-----------------------------------------\n";
 		system("sudo yum update");
 		system("sudo yum autoremove");
 	}
 
 	### Check for pacman
 	elsif (-e "/usr/bin/pacman") {
-		print "pacman\n";
+		print "pacman package manager detected\n";
+		print "-----------------------------------------\n";
 		system("sudo pacman -Syu");
 	}
 
 	### Check for zypper
 	elsif (-e "/usr/bin/zypper") {
-		print "zypper\n";
+		print "zypper package manager detected\n";
+		print "-----------------------------------------\n";
 		system("sudo zypper update");
 	}
 
 	### Else something else 
 	else {
-		print "Something else is installed, update manually";
+		print "ISSUE"
+		print "-----------------------------------------\n";
+		print "update manually";
 	}
 }
 
